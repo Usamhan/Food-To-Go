5db0355bc22fd79559d47e1b4263cd16d8e6cec4
Usamhan <umeedsamhan@gmail.com>
2023-02-26 02:12:53 +0600

    app drawer added

--- app/src/main/java/com/samhan/foodtogov10/Activities/MainActivity.java
+++ app/src/main/java/com/samhan/foodtogov10/Activities/MainActivity.java
@@ -29,5 +29,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_toBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), nav_drawer_activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
